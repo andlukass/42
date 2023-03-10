9ae41a1d58ad630194295948f0b6ea55d8822c24
Lucas Lopes De Andrade <llopes-d@c1r15s3.42lisboa.com>
2023-03-10 13:54:26 +0000

    c02/ex09

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -13,15 +13,15 @@
 
 int	is_alpha(char c)
 {
-	if(c >= 48 || c <= 39)
+	if(c >= 48 && c <= 57)
 	{
 		return (1);
 	}
-	if(c >= 65 || c <= 90)
+	if(c >= 65 && c <= 90)
 	{
 		return (2);
 	}
-	if(c >= 97 || c <= 122)
+	if(c >= 97 && c <= 122)
 	{
 		return (2);
 	}
@@ -36,20 +36,31 @@ int	is_alpha(char c)
 char	*ft_strcapitalize(char *str)
 {
 	int	index;
+	int first;
 
 	index = 0;
-	if (is_alpha(str[index]) >= 97 && str[index] <= 122)
+	if (str[index] >= 97 && str[index] <= 122)
 	{
 		str[index] = (str[index] - 32);
+		first = 1;
 	}
 	while (str[index] != '\0')
-	{
-		if (is_alpha(str[index-1]) != 2)
+	{ 
+		if (index != 0)
+		{
+			first = 0;
+		}
+		if (is_alpha(str[index-1]) == 0)
 		{
 			if (str[index] >= 97 && str[index] <= 122)
 			{
 				str[index] = (str[index] - 32);
+				first = 1;
 			}
+		}
+		if(first == 0 && str[index] >= 65 && str[index] <= 90)
+		{
+			str[index] = (str[index] + 32);
 		}
 		index++;
 	}
@@ -58,6 +69,6 @@ char	*ft_strcapitalize(char *str)
 
 int main()
 {
-	char teste[60] = "ola, tudo bem? 42palavras quarenta-e-duas; cinquenta+e+um";
+	char teste[60] = "/la, tudo bem? 42palaVRas quar_enta-e-duas; cinquenta+e+um";
 	printf("%s\n",ft_strcapitalize(teste));
 }
