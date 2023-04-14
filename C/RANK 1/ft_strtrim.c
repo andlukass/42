0fcaf992569404380eb17f35631c0d975e19ba5d
Looopees <lopess.andrade@gmail.com>
2023-04-14 09:27:24 +0100

    some stuff

---
+++ C/RANK 1/ft_strtrim.c
@@ -0,0 +1,10 @@
+char *ft_strtrim(char const *s1, char const *set)
+{
+}
+
+/*
+int main()
+{
+    ft_strtrim("testee1","testee2");
+}
+*/
--- C/RANK 1/libft.h
+++ C/RANK 1/libft.h
@@ -55,4 +55,6 @@ void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 
 void *ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n);
 
+char *ft_strtrim(char const *s1, char const *set);
+
 #endif
