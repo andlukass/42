dab8b8da313cc24d80a5f708896d3b3184d190f7
Lucas Andrade <lopess.andrade@gmail.com>
2023-06-18 15:54:19 +0100

    edit main

--- C/RANK1/get_next_line/main.c
+++ C/RANK1/get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 21:56:39 by llopes-d          #+#    #+#             */
-/*   Updated: 2023/06/18 15:06:25 by llopes-d         ###   ########.fr       */
+/*   Updated: 2023/06/18 15:54:04 by llopes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,61 +17,26 @@
 #include <string.h>
 #include "get_next_line.h"
 
-void teste1(char *buffer)
-{
-	int	index;
-	int	flag;
-	int	start;
-
-	index = 0;
-	flag = 0;
-	start = 0;
-	while (buffer[index])
-	{
-		if (buffer[index] == '\n' && !flag++)
-			index++;
-		if (flag)
-			buffer[start++] = buffer[index];
-		index++;
-	}
-	while (buffer[start])
-		buffer[start++] = '\0';
-}
-
-
-void teste2(char *line)
-{
-	int	index;
-
-	index = 0;
-	while (line[index] && line[index] != '\n')
-		index++;
-	while (line[++index])
-		line[index] = '\0';
-}
-
 int	main()
 {
-	// int		fd;
-	// char	*t;
-	// int		flag;
+	int		fd;
+	char	*t;
+	int		flag;
 
-	// flag = 0;
-	// fd = open("teste.txt", O_RDONLY);
-	// for (int i = 0; i < 7; i++)
-	// {
-	// 	if (flag)
-	// 		break;
-	// 	t = get_next_line(fd);
-	// 	printf("\nlinha %d: %s", i+1, t);
-	// 	if (!t)
-	// 		flag = 1;
-	// 	//free(t);
-	// }
-	// //printf("linha: %s", get_next_line(fd));
-	// close(fd);
-	// return (0);
-	char teste[] = "vai da o cu mano";
-	teste2(teste);
-	printf("%s\n", teste);
+	flag = 0;
+	fd = open("teste.txt", O_RDONLY);
+	for (int i = 0; i < 7; i++)
+	{
+		if (flag)
+			break;
+		t = get_next_line(fd);
+		printf("\nlinha %d: %s", i+1, t);
+		if (!t)
+			flag = 1;
+		//free(t);
+	}
+	//printf("linha: %s", get_next_line(fd));
+	close(fd);
+	return (0);
+	//printf("buffer size: %d\n", BUFFER_SIZE);
 }
