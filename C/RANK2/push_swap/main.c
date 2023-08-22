0fb2293ed1d15775b7f85808905540053fcb7a81
Lucas Andrade <lopess.andrade@gmail.com>
2023-08-22 20:24:31 +0100

    add indexes

--- C/RANK2/push_swap/main.c
+++ C/RANK2/push_swap/main.c
@@ -24,10 +24,46 @@ int	valid_arg(char *arg)
 	return (OK);
 }
 
-int	init_stacks(t_data *data, int argc, char * argv[])
+int	*get_indexes(int *stack, int length)
+{
+	int	array[length];
+	int	*ori;
+	int	index;
+
+	ori = malloc(sizeof(stack[0]) * length);
+	index = 0;
+	for (int i = 0; i < length - 1; ++i) {
+	ori[i] = stack[i];
+    array[i] = stack[i];
+	}
+	    for (int i = 0; i < length - 1; ++i) {
+        for (int j = i + 1; j < length; ++j) {
+            if (array[i] > array[j]) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < length; ++i) {
+        for (int j = 0; j < length; ++j) {
+            if (ori[i] == array[j]) {
+                ori[i] = j;
+                break;
+            }
+        }
+    }
+	return ori;
+}
+
+int	init_stacks(t_data *data, int argc, char *argv[])
 {
 	int index;
+	int array[argc - 1];
+	int *final;
 	long int value;
+
 
 	index = 1; // PRIMEIRO ARGUMENTO É O PROGRAMA
 	data->stack_a = NULL;
@@ -37,9 +73,16 @@ int	init_stacks(t_data *data, int argc, char * argv[])
 		if (!valid_arg(argv[index]))
 			return (ERROR);
 		value = ft_atoi(argv[index]);
+		array[index - 1] = value;
 		add_to_stack_last(&data->stack_a, new_stack_value(value));
 		index++;
 	}
+	final = get_indexes(array, argc - 1);
+	for (int i = 0; i < argc - 1; ++i) {
+    	printf("%d ", final[i]);
+    }
+	free(final);
+	//get_indexes(data->stack_a, stack_len(data->stack_a));
 	return (OK);
 }
 
